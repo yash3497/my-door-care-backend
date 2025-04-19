327616f9b243960b22b8cb669737b9a32ecb738f
Sonoma <yash.gupta8791133497@gmail.com>
2025-04-19 20:42:15 +0530

    added footer

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="copyright-wrapper">
     <div class="copyright-area">
-        <p>{{__('Developed with ❤️ by ')}} <a href="https://codexcelerate.com/">CodeXcelerate</a></p>
+        <p>{{__('Developed with ❤️ by ')}} <a href="https://mydoorcare.com/">Mydoor Care</a></p>
         <p>{{ $basic_settings->site_name }} <span>{{ $basic_settings->web_version }}</span></p>
     </div>
 </div>
